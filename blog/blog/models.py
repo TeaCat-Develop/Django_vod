cbcc3cc4d990034b5c6aaa87da561ae224116b7e
TeaCat-Neya <TeaCat0416@gmail.com>
2025-09-22 13:29:55 +0900

    📝 blog 앱 모델 추가 및 초기 마이그레이션 생성

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    CATEGORY_CHOICES = (
+        #(db에 들어갈 이름, 실제 표시될 이름)
+        ('free','자유'),
+        ('travel','여행'),
+        ('cat','고양이'),
+        ('dog','강아지'),
+    )
+    category = models.CharField('카테고리', max_length=10, choices=CATEGORY_CHOICES)
+    title = models.CharField('제목', max_length=100)
+    content = models.TextField('본문') # textfield는 글자 제한 x
+
+    created_at = models.DateTimeField('작성일자', auto_now_add=True)
+    updated_at = models.DateTimeField('수정일자',auto_now=True)
+
+# 제목
+# 본문
+# 작성자 => 패스 (추후 업데이트)
+# 작성일자
+# 수정일자
+# 카테고리
+
+
