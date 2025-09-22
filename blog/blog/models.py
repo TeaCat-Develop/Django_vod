a8cc954d642c3eac077fc1219e196d215123fb2b
TeaCat-Neya <TeaCat0416@gmail.com>
2025-09-22 14:35:18 +0900

    📝 Blog 모델 추가 및 admin 등록
    - Blog 앱의 모델 정의 완료
    - Admin 사이트에서 Blog 관리 가능하도록 등록
    - settings.py 일부 설정 조정

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Blog(models.Model):
     CATEGORY_CHOICES = (
         #(db에 들어갈 이름, 실제 표시될 이름)
+        #(category,category_display)
         ('free','자유'),
         ('travel','여행'),
         ('cat','고양이'),
@@ -16,6 +17,12 @@ class Blog(models.Model):
     created_at = models.DateTimeField('작성일자', auto_now_add=True)
     updated_at = models.DateTimeField('수정일자',auto_now=True)
 
+    def __str__(self):
+        return f'[{self.get_category_display()}] {self.title[:10]}'
+
+    class Meta:
+        verbose_name = '블로그'
+        verbose_name_plural = '블로그 목록'
 # 제목
 # 본문
 # 작성자 => 패스 (추후 업데이트)
